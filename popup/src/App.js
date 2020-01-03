8032874e92277312492aca7f8bbab1108a03859a
mhrrmk <mkocadere@gmail.com>
2020-01-03 22:16:33 +0300

    add lowdb and global chrome eslint config to popup

--- popup/src/App.js
+++ popup/src/App.js
@@ -1,24 +1,44 @@
+/*global chrome*/
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import low from 'lowdb';
+import LocalStorage from 'lowdb/adapters/LocalStorage';
+
+const adapter = new LocalStorage('db')
+//const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+// [changes] field from DB
+let changes = db.get("changes")
+
+const commitOnClick = () => {
+  // this button will save changes as a commit and then
+  // reset it
+  console.log("committed:)")
+
+  // TODO: save current changes as a commit to commits in DB
+
+  // delete closed tabs
+  changes.remove({closed: true}).write()
+
+  // reset the fields
+  changes.each(tab => {
+    tab.opened = false
+    tab.changed = false
+  }).write()
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={()=>{chrome.tabs.create({url: "../mainpage/index.html"}, (tab)=>{})}}
+        >mainpage
+      </button>
+      <button
+        onClick={commitOnClick}
+        >commit
+      </button>
     </div>
   );
 }
